24672ebb9ea610945cccc33c261df290c0c80f70
yuya.takahashi <kandenbiribiri@yahoo.co.jp>
2022-04-13 06:14:49 +0000

    commit

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,10 +15,10 @@ class TasksController extends Controller
      */
     public function index()
     {
-             // メッセージ一覧を取得
+             // タスク一覧を取得
         $tasks = Task::all();
 
-        // メッセージ一覧ビューでそれを表示
+        // タスク一覧ビューでそれを表示
         return view('tasks.index', [
             'tasks' => $tasks,
         ]);
@@ -33,7 +33,7 @@ class TasksController extends Controller
     {
          $task = new Task;
 
-        // メッセージ作成ビューを表示
+        // タスク作成ビューを表示
         return view('tasks.create', [
             'task' => $task,
         ]);
@@ -47,7 +47,7 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-               // メッセージを作成
+               // タスクを作成
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -64,11 +64,11 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-               // idの値でメッセージを検索して取得
+               // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
 
-        // メッセージ詳細ビューでそれを表示
-        return view('task.show', [
+        // タスク詳細ビューでそれを表示
+        return view('tasks.show', [
             'task' => $task,
         ]);
     }
@@ -81,11 +81,11 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-             // idの値でメッセージを検索して取得
+             // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
 
         // メッセージ編集ビューでそれを表示
-        return view('task.edit', [
+        return view('tasks.edit', [
             'task' => $task,
         ]);
     }
@@ -99,9 +99,9 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-              // idの値でメッセージを検索して取得
+              // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // メッセージを更新
+        // タスクを更新
         $task->content = $request->content;
         $task->save();
 
@@ -117,9 +117,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-                // idの値でメッセージを検索して取得
+                // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // メッセージを削除
+        // タスクを削除
         $task->delete();
 
         // トップページへリダイレクトさせる
